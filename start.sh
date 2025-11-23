0d6eb592739571d866365c8e8077c002930bb3ee
Thao225 <buithienhuongthao2005@gmail.com>
2025-11-24 00:02:48 +0700

    file sh

--- start.sh
+++ start.sh
@@ -2,11 +2,9 @@
 python -m venv venv
 source venv/Scripts/activate
 pip install -r requirements.txt
-
-python manage.py loaddata data.json
-python manage.py migrate
-
 cd backend
+python manage.py migrate
+python manage.py loaddata data.json
 python manage.py runserver &
 cd ../frontend
 npm install
